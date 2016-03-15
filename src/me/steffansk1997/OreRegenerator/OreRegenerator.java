19595be573d2e808a0f4e553a0635bee5a75a270
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-03-15 03:24:38 +0100

    Switched to WorldGuard and fixed database insert race condition with the ID column

--- src/me/steffansk1997/OreRegenerator/OreRegenerator.java
+++ src/me/steffansk1997/OreRegenerator/OreRegenerator.java
@@ -5,60 +5,48 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.mewin.WGCustomFlags.WGCustomFlagsPlugin;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 
-public class OreRegenerator extends JavaPlugin{
+public class OreRegenerator extends JavaPlugin {
 	public final SQLManager sql = new SQLManager(this);
 	public final EventListener el = new EventListener(this);
-	private WGCustomFlagsPlugin pluginWGCustomFlags;
 	private WorldGuardPlugin pluginWorldGuard;
-	public static final StateFlag FLAG_REGENORES = new StateFlag("regen-ores", false);
-	
+	public static final StateFlag FLAG_REGENORES = new StateFlag("regen-ores",
+			false);
+
 	@Override
-	public void onEnable(){
+	public void onEnable() {
 		this.saveDefaultConfig();
 		this.getServer().getPluginManager().registerEvents(el, this);
 		sql.initDatabase();
 		startCheck();
-		if(getConfig().getString("mode").equalsIgnoreCase("flag")){
-			this.pluginWGCustomFlags = this.setWGCustomFlags();
-			this.pluginWorldGuard = this.setWG();
-			this.pluginWGCustomFlags.addCustomFlag(FLAG_REGENORES);
-		}
 	}
+
 	@Override
-	public void onDisable(){
+	public void onDisable() {
 		sql.closeConnection();
 	}
-	
-	private void startCheck(){
+
+	private void startCheck() {
 		new BukkitRunnable() {
 			@Override
 			public void run() {
 				sql.check();
-			}			
-		}.runTaskTimerAsynchronously(this, 0L, getConfig().getInt("interval")*20L);
+			}
+		}.runTaskTimerAsynchronously(this, 0L,
+				getConfig().getInt("interval") * 20L);
 	}
-	public WGCustomFlagsPlugin setWGCustomFlags(){
-		Plugin wgcf = Bukkit.getPluginManager().getPlugin("WGCustomFlags");
-		if((wgcf == null) || (!(wgcf instanceof WGCustomFlagsPlugin))){
-			return null;
-		}
-		return (WGCustomFlagsPlugin) wgcf;
-	}
-	public WorldGuardPlugin setWG(){
+
+	public WorldGuardPlugin setWG() {
 		Plugin wg = Bukkit.getPluginManager().getPlugin("WorldGuard");
-		if((wg == null) || (!(wg instanceof WorldGuardPlugin))){
+		if ((wg == null) || (!(wg instanceof WorldGuardPlugin))) {
 			return null;
 		}
 		return (WorldGuardPlugin) wg;
 	}
-	public WorldGuardPlugin getWG(){
+
+	public WorldGuardPlugin getWG() {
 		return this.pluginWorldGuard;
-	}
-	public WGCustomFlagsPlugin getWGCF(){
-		return this.pluginWGCustomFlags;
 	}
 }
--- src/me/steffansk1997/OreRegenerator/SQLManager.java
+++ src/me/steffansk1997/OreRegenerator/SQLManager.java
@@ -20,46 +20,66 @@ public class SQLManager {
 	public SQLManager(final OreRegenerator plugin) {
 		this.plugin = plugin;
 	}
-	
-	private void openConnection(){
+
+	private void openConnection() {
 		try {
-			if(plugin.getConfig().getString("databasetype").equalsIgnoreCase("mysql")){
-				this.connection = DriverManager.getConnection("jdbc:mysql://" + this.plugin.getConfig().getString("MySQL.host") + ":" + this.plugin.getConfig().getString("MySQL.port") + "/" + this.plugin.getConfig().getString("MySQL.database"), new StringBuilder().append(this.plugin.getConfig().getString("MySQL.user")).toString(), new StringBuilder().append(this.plugin.getConfig().getString("MySQL.password")).toString());
-			}else{
+			if (plugin.getConfig().getString("databasetype")
+					.equalsIgnoreCase("mysql")) {
+				this.connection = DriverManager.getConnection(
+						"jdbc:mysql://"
+								+ this.plugin.getConfig().getString(
+										"MySQL.host")
+								+ ":"
+								+ this.plugin.getConfig().getString(
+										"MySQL.port")
+								+ "/"
+								+ this.plugin.getConfig().getString(
+										"MySQL.database"),
+						new StringBuilder()
+								.append(this.plugin.getConfig().getString(
+										"MySQL.user")).toString(),
+						new StringBuilder().append(
+								this.plugin.getConfig().getString(
+										"MySQL.password")).toString());
+			} else {
 				Class.forName("org.sqlite.JDBC");
-				this.connection = DriverManager.getConnection("jdbc:sqlite:" + this.plugin.getDataFolder() + "/data.db");
+				this.connection = DriverManager.getConnection("jdbc:sqlite:"
+						+ this.plugin.getDataFolder() + "/data.db");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public void closeConnection(){
+
+	public void closeConnection() {
 		try {
 			this.connection.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void initDatabase(){
-		if(!plugin.getConfig().getString("databasetype").equalsIgnoreCase("mysql")){
+
+	public void initDatabase() {
+		if (!plugin.getConfig().getString("databasetype")
+				.equalsIgnoreCase("mysql")) {
 			try {
 				DriverManager.registerDriver(new org.sqlite.JDBC());
 			} catch (SQLException e1) {
 				e1.printStackTrace();
 			}
 			File dbFile = new File(this.plugin.getDataFolder() + "/data.db");
-			if(!dbFile.exists()){
-	            try {
-	                dbFile.createNewFile();
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
+			if (!dbFile.exists()) {
+				try {
+					dbFile.createNewFile();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		this.openConnection();
 		try {
-			final PreparedStatement sql = connection.prepareStatement("CREATE TABLE IF NOT EXISTS `OreRegen-Blocks` (`ID` INT NOT NULL UNIQUE, `material` varchar(100), `respawntime` INT, `data` INT, `x` INT, `y` INT, `z` INT, `world` VARCHAR(255), PRIMARY KEY (`ID`)) ;");
+			final PreparedStatement sql = connection
+					.prepareStatement("CREATE TABLE IF NOT EXISTS `OreRegen-Blocks` (`ID` ROWID, `material` varchar(100), `respawntime` INT, `data` INT, `x` INT, `y` INT, `z` INT, `world` VARCHAR(255)) ;");
 			sql.execute();
 			sql.close();
 		} catch (Exception e) {
@@ -67,99 +87,114 @@ public class SQLManager {
 			return;
 		}
 	}
-	public void removeItem(int id){
-		try{
-			PreparedStatement sql = connection.prepareStatement("DELETE FROM `OreRegen-Blocks` WHERE `ID`=?;");
+
+	public void removeItem(int id) {
+		try {
+			PreparedStatement sql = connection
+					.prepareStatement("DELETE FROM `OreRegen-Blocks` WHERE `ID`=?;");
 			sql.setInt(1, id);
 			sql.execute();
 			sql.close();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public void insertBlock(final String material, final int data, final int x, final int y, final int z, final String world, final int respawntime){
+
+	public void insertBlock(final String material, final int data, final int x,
+			final int y, final int z, final String world, final int respawntime) {
+
 		new BukkitRunnable() {
 			@Override
-			public void run() {	
-				try{
-					PreparedStatement sql = connection.prepareStatement("INSERT INTO `OreRegen-Blocks` (`id`, `material`, `respawntime`, `data`, `x`, `y`, `z`, `world`) VALUES (?,?,?,?,?,?,?,?);");
-					sql.setInt(1, nextID());
-					sql.setString(2, material);
-					sql.setInt(3, respawntime);
-					sql.setInt(4, data);
-					sql.setInt(5, x);
-					sql.setInt(6, y);
-					sql.setInt(7, z);
-					sql.setString(8, world);
+			public void run() {
+				try {
+					PreparedStatement sql = connection
+							.prepareStatement("INSERT INTO `OreRegen-Blocks` (`material`, `respawntime`, `data`, `x`, `y`, `z`, `world`) VALUES (?,?,?,?,?,?,?);");
+					// sql.setInt(1, nextID());
+					sql.setString(1, material);
+					sql.setInt(2, respawntime);
+					sql.setInt(3, data);
+					sql.setInt(4, x);
+					sql.setInt(5, y);
+					sql.setInt(6, z);
+					sql.setString(7, world);
 					sql.execute();
 					sql.close();
-				}catch(Exception e){
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			}	
+			}
 		}.runTaskAsynchronously(plugin);
+
 	}
-	public String getBlockData(String field, String world, int x, int y, int z){
-		try{
-			PreparedStatement sql = connection.prepareStatement("SELECT * FROM `OreRegen-Blocks` WHERE `world`=? AND `x`=? AND `y`=? AND `z`=?;");
+
+	public String getBlockData(String field, String world, int x, int y, int z) {
+		try {
+			PreparedStatement sql = connection
+					.prepareStatement("SELECT * FROM `OreRegen-Blocks` WHERE `world`=? AND `x`=? AND `y`=? AND `z`=?;");
 			sql.setString(1, world);
 			sql.setInt(2, x);
 			sql.setInt(3, y);
 			sql.setInt(4, z);
 			ResultSet rs = sql.executeQuery();
-			if(rs.next()){
+			if (rs.next()) {
 				String data = rs.getString(field);
 				rs.close();
 				sql.close();
 				return data;
-			}
-			else{
+			} else {
 				rs.close();
 				sql.close();
 				return null;
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
+
 	@SuppressWarnings("deprecation")
-	public void check(){
-		try{
-			PreparedStatement sql1 = connection.prepareStatement("SELECT * FROM `OreRegen-Blocks`;");
-			final ResultSet rs1 = sql1.executeQuery();
-			while(rs1.next()){
-				PreparedStatement sql3 = connection.prepareStatement("UPDATE `OreRegen-Blocks` SET `respawntime`=? WHERE `id`=?;");
-				sql3.setInt(1, rs1.getInt("respawntime") - plugin.getConfig().getInt("interval"));
+	public void check() {
+		try {
+			PreparedStatement sql1 = connection
+					.prepareStatement("SELECT * FROM `OreRegen-Blocks`;");
+			ResultSet rs1 = sql1.executeQuery();
+			while (rs1.next()) {
+				PreparedStatement sql3 = connection
+						.prepareStatement("UPDATE `OreRegen-Blocks` SET `respawntime`=? WHERE `id`=?;");
+				sql3.setInt(1, rs1.getInt("respawntime")
+						- plugin.getConfig().getInt("interval"));
 				sql3.setInt(2, rs1.getInt("id"));
 				sql3.executeUpdate();
-				if(rs1.getInt("respawntime") - plugin.getConfig().getInt("interval") <= 0){
+				if (rs1.getInt("respawntime")
+						- plugin.getConfig().getInt("interval") <= 0) {
+					String world = rs1.getString("world");
+					int x = rs1.getInt("x");
+					int y = rs1.getInt("y");
+					int z = rs1.getInt("z");
+					int data = rs1.getInt("data");
+					String material = rs1.getString("material");
 					new BukkitRunnable() {
 						@Override
 						public void run() {
 							Location loc = null;
-							try {
-								loc = new Location(Bukkit.getWorld(rs1.getString("world")), rs1.getInt("x"), rs1.getInt("y"), rs1.getInt("z"));
-							} catch (SQLException e2) {
-								e2.printStackTrace();
-							}
-							if(loc != null) {
+							loc = new Location(Bukkit.getWorld(world), x, y, z);
+							if (loc != null) {
 								Block bl = loc.getBlock();
-								try {
-									if(bl.getType() == Material.valueOf(plugin.getConfig().getString("empty").toUpperCase()) || bl.getType() == Material.valueOf(plugin.getConfig().getString("delays." + rs1.getString("material").toUpperCase() + ".empty").toUpperCase())){
-										try {
-											bl.setType(Material.valueOf(rs1.getString("material").toUpperCase()));
-										} catch (SQLException e1) {
-											e1.printStackTrace();
-										}
-										try {
-											bl.setData((byte) rs1.getInt("data"));
-										} catch (SQLException e) {
-											e.printStackTrace();
-										}
-									}
-								} catch (SQLException e) {
-									e.printStackTrace();
+								if (bl.getType() == Material.valueOf(plugin
+										.getConfig().getString("empty")
+										.toUpperCase())
+										|| bl.getType() == Material
+												.valueOf(plugin
+														.getConfig()
+														.getString(
+																"delays."
+																		+ material
+																				.toUpperCase()
+																		+ ".empty")
+														.toUpperCase())) {
+									bl.setType(Material.valueOf(material
+											.toUpperCase()));
+									bl.setData((byte) data);
 								}
 							}
 						}
@@ -170,21 +205,23 @@ public class SQLManager {
 			}
 			rs1.close();
 			sql1.close();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public int nextID(){
+
+	public int nextID() {
 		int data = 0;
-		try{
-			PreparedStatement sql = connection.prepareStatement("SELECT `ID` FROM `OreRegen-Blocks` ORDER BY `ID` DESC LIMIT 1;");
+		try {
+			PreparedStatement sql = connection
+					.prepareStatement("SELECT `ID` FROM `OreRegen-Blocks` ORDER BY `ID` DESC LIMIT 1;");
 			ResultSet rs = sql.executeQuery();
-			if(rs.next()){
+			if (rs.next()) {
 				data = rs.getInt("id") + 1;
 			}
 			rs.close();
 			sql.close();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return data;
